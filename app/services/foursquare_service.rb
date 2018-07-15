51d3817594ed76708464965d508e73bc2ec0b672
Lauren Perchitti <laurenperchitti@gmail.com>
2018-07-15 21:01:21 +0000

    automatically backed up by learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -14,4 +14,13 @@ class FoursquareService
     redirect_to root_path
   end
 
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+
 end
